b52c8deb26fee36bbcfe908f79e5ca86a0b881ad
Rolando Brondolin <rolando.brondolin@mail.polimi.it>
2017-11-13 10:38:07 +0100

    first iteration of bpf aggregation, now testing

---
+++ bpf/bpf_monitor.c
@@ -0,0 +1,166 @@
+struct pid_status {
+        int pid;
+        char comm[16];
+        u64 weighted_cycles[2];
+        // set which item of weighted_cycles should be used in bpf
+        // in user space, the weighted_cycles is read and initialized
+        unsigned int bpf_selector;
+        u64 ts;
+};
+struct proc_topology {
+        u64 ht_id;
+        u64 sibling_id;
+        u64 core_id;
+        u64 processor_id;
+        u64 cycles;
+        u64 ts;
+        int running_pid;
+};
+struct sched_switch_args {
+        __u64 pad; // regs after 4.x?
+        char prev_comm[16];
+        int prev_pid;
+        int prev_prio;
+        long long prev_state;
+        char next_comm[16];
+        int next_pid;
+        int next_prio;
+};
+struct sched_process_exec_args {
+        __u64 pad; // regs after 4.x?
+        char filename[4];
+        int pid;
+        int old_pid;
+};
+struct sched_process_fork_args {
+        __u64 pad; // regs after 4.x?
+        char parent_comm[16];
+        int parent_pid;
+        char child_comm[16];
+        int child_pid;
+};
+struct sched_process_exit_args {
+        __u64 pad; // regs after 4.x?
+        char comm[16];
+        int pid;
+        int prio;
+};
+
+BPF_PERF_OUTPUT(events);
+BPF_PERF_ARRAY(cpu_cycles, NUM_CPUS);
+BPF_HASH(processors, u64, struct proc_topology);
+BPF_HASH(pids, int, struct pid_status);
+BPF_HASH(conf, int, unsigned int);
+
+#define STEP 2000000000
+
+int trace_function(struct sched_switch_args *ctx) {
+        int conf_key = 0;
+        unsigned int bpf_selector = 0;// = conf.lookup(&conf_key);
+        bpf_probe_read(&bpf_selector, sizeof(bpf_selector), conf.lookup(&conf_key));
+
+        if (bpf_selector > 1) {
+                return 0;
+        }
+
+        u64 processor_id = bpf_get_smp_processor_id();
+        u64 cycles = cpu_cycles.perf_read(processor_id);
+        u64 ts = bpf_ktime_get_ns();
+        int old_pid = ctx->prev_pid;
+
+
+        struct pid_status status_old;// = pids.lookup(&(old_pid));
+        bpf_probe_read(&status_old, sizeof(status_old), pids.lookup(&(old_pid)));
+
+        struct proc_topology topology_info;// = processors.lookup(&processor_id);
+        bpf_probe_read(&topology_info, sizeof(topology_info), processors.lookup(&processor_id));
+
+        if(topology_info.ht_id > NUM_CPUS) {
+                return 0;
+        }
+
+        if(status_old.pid != 0) {
+                //find the entry related to processor_id and its sibling
+                u64 sibling_id = 0;
+                bpf_probe_read(&sibling_id, sizeof(sibling_id), &topology_info.sibling_id);
+                struct proc_topology *sibling_info = processors.lookup(&(sibling_id));
+                if(!sibling_info) {
+                        return 0;
+                }
+                u64 old_cycles = (sibling_info->ts > topology_info.ts) ? sibling_info->cycles : topology_info.cycles;
+
+                u64 weight_factor = 1;
+                //find the sibling pid status
+                if(sibling_info->running_pid != 0) {
+                        int sibling_pid = 0;
+                        bpf_probe_read(&sibling_pid, sizeof(sibling_pid), &sibling_info->running_pid);
+                        struct pid_status sibling_process;// = pids.lookup(&(sibling_pid));
+                        bpf_probe_read(&sibling_process, sizeof(sibling_process), pids.lookup(&(sibling_pid)));
+                        if(sibling_process.pid == sibling_pid && sibling_process.pid != 0) {
+                                weight_factor = 10;
+                                if(sibling_process.bpf_selector == 0) {
+                                  sibling_process.weighted_cycles[0] += (cycles - old_cycles) + (cycles - old_cycles)/weight_factor;
+                                  sibling_process.ts = ts;
+                                  pids.update(&(sibling_pid), &sibling_process);
+                                } else if (sibling_process.bpf_selector == 1) {
+                                  sibling_process.weighted_cycles[1] += (cycles - old_cycles) + (cycles - old_cycles)/weight_factor;
+                                  sibling_process.ts = ts;
+                                  pids.update(&(sibling_pid), &sibling_process);
+                                } else {
+                                  return 0;
+                                }
+
+                        } else {
+                          return 0;
+                        }
+                }
+                //increment counters on our pid
+                if(status_old.bpf_selector == 0) {
+                  status_old.weighted_cycles[0] += (cycles - old_cycles) + (cycles - old_cycles)/weight_factor;
+                  status_old.ts = ts;
+                  pids.update(&old_pid, &status_old);
+                } else if (status_old.bpf_selector == 1) {
+                  status_old.weighted_cycles[1] += (cycles - old_cycles) + (cycles - old_cycles)/weight_factor;
+                  status_old.ts = ts;
+                  pids.update(&old_pid, &status_old);
+                } else {
+                  return 0;
+                }
+
+        }
+        //no info on old status, let another enter sched build it
+        int new_pid = ctx->next_pid;
+        struct pid_status status_new;// = pids.lookup(&(new_pid));
+        bpf_probe_read(&status_new, sizeof(status_new), pids.lookup(&(new_pid)));
+
+        //If no status for PID, then create one, otherwise update selector
+        if(status_new.pid != 0) {
+                // here just update the selector and reset counter if needed
+                if(status_new.bpf_selector != bpf_selector || status_new.ts > ts + STEP) {
+                        status_new.bpf_selector = bpf_selector;
+                        if(bpf_selector) {
+                          status_new.weighted_cycles[1] = 0;
+                        } else if (!bpf_selector){
+                          status_new.weighted_cycles[0] = 0;
+                        } else {
+                          return 0;
+                        }
+                        pids.update(&new_pid, &status_new);
+                }
+        } else if(status_new.pid == 0 && new_pid != 0) {
+                bpf_probe_read(&(status_new.comm), sizeof(status_new.comm), ctx->next_comm);
+                status_new.pid = new_pid;
+                status_new.ts = ts;
+                status_new.weighted_cycles[0] = 0;
+                status_new.weighted_cycles[1] = 0;
+                status_new.bpf_selector = bpf_selector;
+                pids.insert(&new_pid, &status_new);
+        }
+
+        //add info on new running pid into processors table
+        topology_info.running_pid = new_pid;
+        topology_info.cycles = cycles;
+        topology_info.ts = ts;
+        processors.update(&processor_id, &topology_info);
+        return 0;
+}
---
+++ dockercap_monitor.py
@@ -0,0 +1,79 @@
+from bcc import BPF, PerfType, PerfHWConfig
+import multiprocessing
+import ctypes as ct
+import os
+import time
+
+TASK_COMM_LEN = 16
+
+class ProcTopology(ct.Structure):
+    _fields_ = [("ht_id", ct.c_ulonglong),
+                ("sibling_id", ct.c_ulonglong),
+                ("core_id", ct.c_ulonglong),
+                ("processor_id", ct.c_ulonglong),
+                ("cycles", ct.c_ulonglong),
+                ("ts", ct.c_ulonglong),
+                ("running_pid", ct.c_int)]
+
+class PidStatus(ct.Structure):
+    _fields_ = [("pid", ct.c_int),
+                ("comm", ct.c_char * TASK_COMM_LEN),
+                ("weighted_cycles", ct.c_ulonglong * 2),
+                ("bpf_selector", ct.c_int),
+                ("ts", ct.c_ulonglong)]
+
+#Load BPF program
+bpf_program = BPF(src_file="bpf/bpf_monitor.c", cflags=["-DNUM_CPUS=%d" % multiprocessing.cpu_count()])
+# Open cycles PMC
+bpf_program["cpu_cycles"].open_perf_event(PerfType.HARDWARE, PerfHWConfig.CPU_CYCLES)
+# get tables
+processors = bpf_program.get_table("processors")
+pids = bpf_program.get_table("pids")
+conf = bpf_program.get_table("conf")
+# set default bpf_selector
+conf[ct.c_int(0)] = ct.c_uint(0)
+
+# parse /proc/cpuinfo to obtain processor topology
+ht_id = 0
+sibling_id = 0
+core_id = 0
+processor_id = 0
+
+coresDict = {} #core elem is organized as ht_id, sibling_id, core_id, processor_id
+
+with open('/proc/cpuinfo') as f:
+    for line in f:
+        sp = line.split(" ")
+        if "processor\t" in sp[0]:
+            ht_id = int(sp[1])
+        if "physical" in sp[0] and "id\t" in sp[1]:
+            processor_id = int(sp[2])
+        if "core" in sp[0] and "id\t\t" in sp[1]:
+            core_id = int(sp[2])
+            found = False
+            for key, value in coresDict.iteritems():
+                if value[2] == core_id:
+                    found = True
+                    value[1] = ht_id
+                    coresDict[ht_id] = [ht_id, value[0], core_id, processor_id]
+                    break
+            if not found:
+                coresDict[ht_id] = [ht_id, -1, core_id, processor_id]
+
+# populate processors hash with proc topology
+for key, value in coresDict.iteritems():
+    core = ProcTopology(ct.c_ulonglong(value[0]), ct.c_ulonglong(value[1]),     \
+        ct.c_ulonglong(value[2]), ct.c_ulonglong(value[3]), ct.c_ulonglong(0),  \
+        ct.c_ulonglong(0), ct.c_int(0))
+    processors[ct.c_ulonglong(key)] = core
+
+# attach TRACEPOINT
+bpf_program.attach_tracepoint(tp="sched:sched_switch", fn_name="trace_function")
+# sleep and retrieve data
+while True:
+    time.sleep(2)
+    if conf[ct.c_int(0)] == 0:
+        conf[ct.c_int(0)] = ct.c_int(1)
+
+    else:
+        conf[ct.c_int(0)] = ct.c_int(1)
