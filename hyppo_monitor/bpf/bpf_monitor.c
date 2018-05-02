8a85c5b39177080735b535d124826c99cd7118d2
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2018-05-02 23:45:32 +0200

    Add comments to bpf_monitor.c

--- hyppo_monitor/bpf/bpf_monitor.c
+++ hyppo_monitor/bpf/bpf_monitor.c
@@ -1,17 +1,41 @@
-#define SELECTOR_DIM 2
+/**
+ * In the rest of the code we are going to use a selector to read and write
+ * events. The idea is that while userspace is reading events
+ * for selector 0 we write events using selector 1 and vice versa.
+ * This process in handled by the bpf_selector variable.
+ */
+#define SELECTOR_DIM 2 // Use a binary selector
+/**
+ * Slots are array cells we use to store events following the selector idea.
+ * Let's take as an example NUM_SOCKETS=2 (2 physical CPUs) and let's keep
+ * SELECTOR_DIM=2 as defined above. In this case the array would have four slots
+ * and will be partitioned in the following way:
+ * [ CPU_0 SELECTOR_0 | CPU_0 SELECTOR_1 | CPU_1 SELECTOR_0 | CPU_1 SELECTOR_1 ]
+ */
 #define NUM_SLOTS NUM_SOCKETS * SELECTOR_DIM
 
+/**
+ * pid_status is used to store information about pid X
+ * bpf_selector is used for the slot selection described above and is set
+ * in the BPF context. PCM arrays like cycles and IR are written by BPF but
+ * read and initialized from user space.
+ */
 struct pid_status {
-        int pid;
-        char comm[16];
-        u64 weighted_cycles[NUM_SLOTS];
-        u64 instruction_retired[NUM_SLOTS];
-        u64 time_ns[NUM_SLOTS];
-        // set which item of weighted_cycles should be used in bpf
-        // in user space, the weighted_cycles is read and initialized
-        unsigned int bpf_selector;
-        u64 ts[NUM_SLOTS];
+        int pid;                            /**< Process ID */
+        char comm[16];                      /**< Process name */
+        u64 weighted_cycles[NUM_SLOTS];     /**< Number of weighted cycles executed by the process */
+        u64 instruction_retired[NUM_SLOTS]; /**< Number of instructions executed by the process */
+        u64 time_ns[NUM_SLOTS];             /**< Execution time of the process (in ns) */
+        unsigned int bpf_selector;          /**< Slot selector */
+        u64 ts[NUM_SLOTS];                  /**< Timestamp of the latest update */
 };
+
+/**
+ * proc_topology is used to store information about the underlying topology
+ * of CPUS.
+ * We distinguish processors, cores (physical core) and ht (soft cores).
+ * Siblings cores are two ht cores residing on the same physical core.
+ */
 struct proc_topology {
         u64 ht_id;
         u64 sibling_id;
@@ -24,6 +48,11 @@ struct proc_topology {
         u64 ts;
         int running_pid;
 };
+
+/**
+ * sched_switch_args is the payload of a sched_switch tracepoint event
+ * as defined in the Linux kernel.
+ */
 struct sched_switch_args {
         __u64 pad; // regs after 4.x?
         char prev_comm[16];
@@ -68,9 +97,21 @@ BPF_PERF_ARRAY(instr_thread, NUM_CPUS);
 BPF_HASH(processors, u64, struct proc_topology);
 BPF_HASH(pids, int, struct pid_status);
 BPF_HASH(idles, u64, struct pid_status);
+/**
+ * conf struct has 4 integer keys initialized in user space
+ * 0: current bpf selector
+ * 1: old bpf selector
+ * 2: timeslice (dynamic window duration)
+ * 3: switch count (global context switch counter). Used to compute the window size
+ */
 BPF_HASH(conf, int, unsigned int);
 
-// Beware: Changing the step in userspace means invalidate the last sample
+/**
+ * STEP_MIN and STEP_MAX are the lower and upper bound for the duration
+ * of the dynamic window (interval between two reads from user space)
+ * They are expressed in nanoseconds so their range is 1-4 second.
+ * BEWARE: Changing the step in userspace means invalidate the last sample
+ */
 #define STEP_MIN 1000000000
 #define STEP_MAX 4000000000
 
@@ -88,28 +129,35 @@ static void send_error(struct sched_switch_args *ctx, int err_code) {
 
 int trace_switch(struct sched_switch_args *ctx) {
 
+        // Keys for the conf hash
         int selector_key = 0;
         int old_selector_key = 1;
         int step_key = 2;
         int switch_count_key = 3;
 
+        // Slot iterator for the selector
         int array_index = 0;
 
+        // Binary selector to avoid event overwriting
         unsigned int bpf_selector = 0;
         int ret = 0;
         ret = bpf_probe_read(&bpf_selector, sizeof(bpf_selector), conf.lookup(&selector_key));
-        // if selector is not in place correctly, signal debug error and stop tracing routine
+        // If selector is not in place correctly, signal debug error and stop tracing routine
         if (ret!= 0 || bpf_selector > 1) {
                 send_error(ctx, -1);
                 return 0;
         }
 
-        // retrieve general switch count
+        // Retrieve general switch count
         unsigned int switch_count = 0;
         ret = 0;
         ret = bpf_probe_read(&switch_count, sizeof(switch_count), conf.lookup(&switch_count_key));
 
-        //retrieve old selector to update switch count correctly
+        /**
+         * Retrieve old selector to update switch count correctly
+         * If the current selector is still active increase the switch count
+         * otherwise reset the count and update the current selector
+         */
         unsigned int old_bpf_selector = 0;
         ret = 0;
         ret = bpf_probe_read(&old_bpf_selector, sizeof(old_bpf_selector), conf.lookup(&old_selector_key));
@@ -124,10 +172,12 @@ int trace_switch(struct sched_switch_args *ctx) {
         }
         conf.update(&switch_count_key, &switch_count);
 
-        // retrieve sampling step
-        // Beware: Increasing the step in userspace means that the next sample is invalid
-        // Reducing the step in userspace is not an issue, give that it excludes data
-        // that is inside an can be discarded
+        /**
+         * Retrieve sampling step (dynamic window)
+         * BEWARE: increasing the step in userspace means that the next
+         *         sample is invalid. Reducing the step in userspace is not
+         *         an issue, it discards data that has already been collected
+         */
         unsigned int step = 1000000000;
         ret = bpf_probe_read(&step, sizeof(step), conf.lookup(&step_key));
         if (ret!= 0 || step < STEP_MIN || step > STEP_MAX) {
@@ -136,8 +186,11 @@ int trace_switch(struct sched_switch_args *ctx) {
         }
 
 
-        // get data about processor and performance counters
-        // lookup also the pid of the exiting process
+        /**
+         * Get the id of the processor where the sched_switch happened.
+         * Collect cycles and IR samples from perf arrays.
+         * Save the timestamp and store the exiting pid
+         */
         u64 processor_id = bpf_get_smp_processor_id();
         u64 thread_cycles_sample = cycles_thread.perf_read(processor_id);
         u64 core_cycles_sample = cycles_core.perf_read(processor_id);
@@ -145,7 +198,7 @@ int trace_switch(struct sched_switch_args *ctx) {
         u64 ts = bpf_ktime_get_ns();
         int old_pid = ctx->prev_pid;
 
-        // fetch data about processor executing the thing
+        // Fetch more data about processor where the sched_switch happened
         struct proc_topology topology_info;
         ret = bpf_probe_read(&topology_info, sizeof(topology_info), processors.lookup(&processor_id));
         if(ret!= 0 || topology_info.ht_id > NUM_CPUS) {
@@ -153,11 +206,14 @@ int trace_switch(struct sched_switch_args *ctx) {
                 return 0;
         }
 
-        // fetch the status of the exiting pid
+        // Create the struct to hold the pid status we are going to fetch
         struct pid_status status_old;
         status_old.pid = -1;
 
-        // if the pid is 0, then use the idles perf_hash
+        /**
+         * Fetch the status of the exiting pid.
+         * If the pid is 0 then use the idles perf_hash
+         */
         if(old_pid == 0) {
                 ret = bpf_probe_read(&status_old, sizeof(status_old), idles.lookup(&(processor_id)));
         } else {
@@ -165,36 +221,38 @@ int trace_switch(struct sched_switch_args *ctx) {
         }
 
         if(ret == 0) {
-                //
                 // Retrieving information of the sibling processor
-                //
                 u64 sibling_id = topology_info.sibling_id;
                 struct proc_topology sibling_info;
                 ret = bpf_probe_read(&sibling_info, sizeof(sibling_info), processors.lookup(&(sibling_id)));
 
                 if(ret != 0) {
-                        // wrong info on topology, do nothing
+                        // Wrong info on topology, do nothing
                         send_error(ctx, 3);
                         return 0;
                 }
 
-                //
-                // Instead of adding stuff directly, given that we don't have the measure of the sibling thread cycles,
-                // We are summing up the information on our side to the core cycles of the sibling
-                //
-                //update sibling process info
+                /**
+                 * Instead of adding stuff directly, given that we don't have
+                 * the measure of the sibling thread cycles, we are summing up
+                 * the information on our side to the core cycles of the sibling
+                 */
+                // Update sibling process info
                 if(sibling_info.running_pid > 0 && old_pid > 0 && core_cycles_sample > sibling_info.cycles_core) {
                         sibling_info.cycles_core_delta_sibling += core_cycles_sample - sibling_info.cycles_core;
                 }
                 sibling_info.cycles_core = core_cycles_sample;
                 processors.update(&sibling_id, &sibling_info);
 
-                //
-                // Get back to our pid and our processor
-                // Update the data for proc_topology and pid info
-                //
+                /**
+                 * Get back to our pid and our processor
+                 * Update the data for proc_topology and pid info
+                 */
                 u64 last_ts_pid_in = 0;
-                //trick the compiler with loop unrolling
+                // Trick the bpf compiler with loop unrolling
+                // ??? last_ts_pid_in = status_old.ts[status_old.bpf_selector
+                //                                    + SELECTOR_DIM
+                //                                    * topology_info.processor_id];
                 #pragma clang loop unroll(full)
                 for(array_index = 0; array_index<NUM_SLOTS; array_index++) {
                         if(array_index == status_old.bpf_selector + SELECTOR_DIM * topology_info.processor_id) {
