d079c0f86883afe2b08eaac075f375f9c9afdf33
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2018-10-12 15:26:19 +0200

    Remove legacy GRPC code and files

--- setup.py
+++ setup.py
@@ -17,11 +17,3 @@ setup(
     #license='Creative Commons Attribution-Noncommercial-Share Alike license',
     #long_description=open('README.txt').read(),
 )
-
-setup(
-    name='hyppo-proto',
-    version='0.1dev',
-    packages=['hyppo_proto'],
-    #license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    #long_description=open('README.txt').read(),
-)
