a20484e7c3a101b0e02e7e46f9379dd01c9a5aac
Rolando Brondolin <rolando.brondolin@mail.polimi.it>
2019-08-07 23:50:38 +0200

    add customer data for kubestate

--- snap/plugins/snap_plugin_publisher_kubernetes/snap_plugin_publisher_kubernetes.py
+++ snap/plugins/snap_plugin_publisher_kubernetes/snap_plugin_publisher_kubernetes.py
@@ -4,6 +4,13 @@ import json
 import logging
 import snap_plugin.v1 as snap
 import urllib2
+import os
+import time
+import yaml
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
 
 from google.protobuf import json_format
 
@@ -14,7 +21,10 @@ class KubernetesPublisher(snap.Publisher):
 
     def __init__(self, name, version, **kwargs):
         super(KubernetesPublisher, self).__init__(name, version, **kwargs)
-        self.connected = False
+
+        self.config = {}
+        self.customer_id = ""
+
 
     def publish(self, metrics, config):
         LOG.debug("KubernetesPublisher called")
@@ -26,15 +36,35 @@ class KubernetesPublisher(snap.Publisher):
             :obj:`list` of :obj:`snap_plugin.v1.Metric`:
                 List of collected metrics.
         """
+        if self.customer_id == "":
+            #time.sleep(1)
+            try:
+                with open('/hyppo-config/config.yaml', 'r') as config_file:
+                    self.config = yaml.load(config_file)
+            except Exception:
+                try:
+                    with open('hyppo_monitor/config.yaml', 'r') as config_file:
+                        self.config = yaml.load(config_file)
+                except Exception:
+                    LOG.error("Couldn't find a config file, current path is %s", os.getcwd())
+
+            try:
+                self.customer_id = self.config["customer_id"]
+            except KeyError as e:
+                self.customer_id = "not_registered"
+
+
 
         data = []
         for metric in metrics:
             data.append(json_format.MessageToDict(metric._pb, including_default_value_fields=True))
 
+        payload = {"customer_id": self.customer_id, "data": data}
+
         try:
             req = urllib2.Request("http://" + config["remote_collector"] + "/send_kube")
             req.add_header('Content-Type', 'application/json')
-            response = urllib2.urlopen(req, json.dumps(data))
+            response = urllib2.urlopen(req, json.dumps(payload))
         except Exception:
             import traceback
             LOG.error(traceback.format_exc())
